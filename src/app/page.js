d9636fe61853637315a383705da669f68e065c58
Mohamed Diaa <mohameddiaa201996@gmail.com>
2025-08-26 21:35:01 +0300

    Added a functioning delete button

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
+
 'use client'
 import React, { useEffect, useState } from "react"
-
 
 export default function Home() {
   const [task,settask] = useState("")
@@ -15,7 +15,10 @@ const addingtask = () => {
   }
 }
 
-
+const deletetask = (deletetask) => {
+const newtasklist = tasks.filter((_,i) => i !== deletetask)
+settasks(newtasklist)
+}
 
 useEffect(() =>{
   const save = localStorage.getItem("tasks");
@@ -23,10 +26,27 @@ useEffect(() =>{
     settasks(JSON.parse(save));
   }
 }, []);
+
 useEffect(() =>{
   localStorage.setItem("tasks",JSON.stringify(tasks));
 },[tasks]);
+
+
+
 //  console.log(tasks)
+
+// const [datauser, setdatauser] = useState()
+
+// useEffect(() => {
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(data => data.json())
+//     .then((user) => {
+//       setdatauser(user);
+//     })
+// }, []);
+// if (!datauser) return <div>Loading...</div>
+
+
 
 return (
   <div className="w-full h-full flex flex-col items-center">
@@ -54,9 +74,30 @@ return (
 <div className="flex justify-center items-center">
   {/* input value: add user task */}
   <ul id="unlisted" className="list-disc">
-    {tasks.map((inputvalue,index) =>(
-      <li key={index} style={{color:inputvalue.textcolorchange}}>{inputvalue.textandcolor}</li>
-    ))}
+    {tasks.map((inputvalue,index) =>(<li className="list-disc flex gap-5 items-center justify-center" key={index} style={{color:inputvalue.textcolorchange}}>{inputvalue.textandcolor}
+     
+      <button onClick={()=>deletetask(index)}
+  className="inline-flex items-center px-4 cursor-pointer py-0 bg-red-600 transition ease-in-out delay-75 hover:bg-red-700 text-white text-sm font-medium rounded-md hover:-translate-y-1 hover:scale-110">
+  <svg
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    fill="none"
+    className="h-5 w-5 mr-2"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+      strokeWidth="2"
+      strokeLinejoin="round"
+      strokeLinecap="round"
+    ></path>
+  </svg>
+  Delete
+</button>
+
+    </li>))}
+
+
   </ul>
 </div>
 <div className="pt-15"></div>
