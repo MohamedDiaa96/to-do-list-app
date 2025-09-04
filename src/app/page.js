ef28e6f7c82d0021479e0979671163e962960844
Mohamed Diaa <mohameddiaa201996@gmail.com>
2025-09-04 15:46:55 +0300

    updated the task UI and made the tasks editable and also added task color sets change and text on change display when writing a text

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,13 @@ export default function Home() {
 const [tasks,settasks] = useState([])
 const [textcolorchange,settextcolorchange]=useState()
 
+const [taskcolorsets,settaskcolorsets] = useState("#99a1af")
+
 const addingtask = () => {
 
   if(task.trim()=== "") return;
   {
-  settasks([...tasks,{textandcolor:task,textcolorchange,favorites:false}]);
+  settasks([...tasks,{text:task,textcolorchange,taskcolorsets,favorites:false}]);
   settask("")
   }
 }
@@ -46,6 +48,23 @@ const toggleFavorite = (index) => {
   settasks(newTasks)
 };
 
+  const [editIndex, setEditIndex] = useState(null);
+
+const startEditTask = (index) => {
+  setEditIndex(index);
+  settask(tasks[index].text);
+  settextcolorchange(tasks[index].textcolorchange);
+  settaskcolorsets(tasks[index].taskcolorsets);
+};
+
+const saveEditTask = () => {
+  if (editIndex === null || task.trim() === "") return;
+  const updatedTasks = [...tasks];
+  updatedTasks[editIndex] = {...updatedTasks[editIndex],text: task,textcolorchange,taskcolorsets};
+  settasks(updatedTasks);
+  setEditIndex(null);
+  settask("");
+}
 //  console.log(tasks)
 
 // const [datauser, setdatauser] = useState()
@@ -65,32 +84,94 @@ return (
     <div className="border-2 border-black w-120 h-full border-dashed mt-5 bg-gray-100">
 {/* form border  */}
 
-    <div className="h-[30vh] w-[100%] flex justify-center items-center"><h1 className="text-3xl text-cyan-800 font-serif  decoration-2 decoration-solid decoration-cyan-600 underline">To Do List App</h1></div>
+    <div className="h-[30vh] w-[100%] flex justify-center items-center"><h1 className="text-3xl text-cyan-800 font-serif decoration-2 decoration-solid decoration-cyan-600 underline">To Do List App</h1></div>
 {/* header  */}
-<div className="flex justify-center gap-2">
+<div className="flex flex-col justify-center items-center gap-2">
   {/* input and button div */}
-  <input  onChange={(e) => {settask(e.target.value) 
-    console.log(e.target.value)
-  }}  value={task} className="outline-none bg-white rounded-2xl placeholder:text-center border-1" type="text" placeholder="Type Your Task Here"></input>
 
-  <button onClick={()=>{
-    addingtask()
-  //  document.body.style.color=textcolorchange;
-    }} className="bg-purple-600 w-30 h-10 text-white rounded-2xl cursor-pointer">Add Task</button>
+  {/* <input  onChange={(e) => {settask(e.target.value) 
+    console.log(e.target.value)
+  }}  value={task} className="outline-none bg-white rounded-2xl placeholder:text-center border-1" type="text" placeholder="Type Your Task Here"></input> */}
+
+
+  <div className="h-50 w-90 rounded-[10%] shadow-[0px_0px_10px] shadow-gray-500 " style={{backgroundColor:taskcolorsets}}>
+<div className="border-b-1 flex justify-center">Task</div>
+<div className="flex items-center justify-center w-full h-[90%]">
+<input  onChange={(e) => {settask(e.target.value) 
+    console.log(e.target.value)
+  }}  value={task} style={{color:textcolorchange}} className="outline-none rounded-2xl placeholder:text-center size-full" type="text" placeholder="Type Your Task Here"></input>
+  </div>
+  </div>
+
+<div className="flex justify-center items-center flex-col gap-2">
+  <button   onClick={() => {
+    editIndex === null ? addingtask() : saveEditTask();
+  }}
+  className="bg-purple-600 w-30 h-10 text-white rounded-2xl cursor-pointer"
+> {editIndex === null ? "Add Task" : "Save Edit"}</button>
+    <div className="flex justify-center items-center gap-2">
+      <span>Text Color</span>
     <input type="color" onChange={(e) =>{
-    settextcolorchange(e.target.value)
+    settextcolorchange(e.target.value) 
   }}></input>
+  </div>
+
+  <div className="flex items-center justify-center gap-2 ">
+    <span>Task Color</span>
+        <button type="color" className="bg-gray-400 rounded-4xl h-10 w-10 cursor-pointer shadow-[0px_0px_10px] shadow-gray-400" onClick={()=>{
+      settaskcolorsets("#99a1af")
+    }}></button>
+    <button type="color" className="bg-red-500 rounded-4xl h-10 w-10 cursor-pointer shadow-[0px_0px_10px] shadow-red-500" onClick={()=>{
+      settaskcolorsets("#fb2c36")
+    }}></button>
+        <button className="bg-green-500 rounded-4xl h-10 w-10 cursor-pointer shadow-[0px_0px_10px] shadow-green-500" onClick={()=>{
+      settaskcolorsets("oklch(72.3% 0.219 149.579)")
+    }}></button>
+            <button className="bg-blue-500 rounded-4xl h-10 w-10 cursor-pointer shadow-[0px_0px_10px] shadow-blue-500" onClick={()=>{
+      settaskcolorsets("oklch(62.3% 0.214 259.815)")
+    }}></button>
+            <button className="bg-yellow-500 rounded-4xl h-10 w-10 cursor-pointer  shadow-[0px_0px_10px] shadow-yellow-500" onClick={()=>{
+      settaskcolorsets("oklch(79.5% 0.184 86.047)")
+    }}></button>
+  </div>
+
 </div>
 <div className="pt-15"></div>
 
     </div>
-    <div className="flex justify-center items-center pt-15">
+    </div>
+    <div className="flex justify-center items-center pt-15 flex-wrap">
   {/* input value: add user task */}
 
       <ul id="unlisted" className= "flex gap-4">
-    {tasks.map((inputvalue,index) =>(<div key={index} className=" bg-gray-400 h-full ">
-<li className="border-b-1 flex justify-center">Task-{index + 1}</li>
-<li className="list-disc flex gap-5 w-50 h-full items-center justify-center p-4 break-all "  style={{color:inputvalue.textcolorchange}}>{inputvalue.textandcolor}</li>
+    {tasks.map((inputvalue,index) =>(<div key={index} className=" h-full rounded-[10%] shadow-[0px_0px_10px] shadow-gray-500" style={{backgroundColor:inputvalue.taskcolorsets}}>
+<div className="flex border-b-1">
+  <div className="flex w-10 justify-center items-center">
+
+<button
+onClick={()=>startEditTask(index)}
+className="cursor-pointer"
+  // className="inline-flex items-center justify-center px-4 py-2 bg-blue-600 ease-in-out delay-75 hover:bg-blue-700 text-white text-sm font-medium rounded-md hover:-translate-y-1 hover:scale-110 active:scale-95 transition-all duration-200"
+>
+  <svg
+    className="h-5 w-5 mr-1 self-center items-center"
+    fill="none"
+    stroke="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"
+    ></path>
+  </svg>
+</button>
+
+  </div>
+  <div className="flex justify-center items-center w-[60%]">
+<li>Task-{index + 1}</li>
+</div>
+</div>
+<li className="list-disc flex gap-5 w-50 h-full items-center justify-center p-4 break-all "  style={{color:inputvalue.textcolorchange,}}>{inputvalue.text}</li>
+
 <div className="flex border-t-1 items-center">
 <label className="">
   <input type="checkbox" checked={inputvalue.favorites}  onChange={() => toggleFavorite(index)} className="peer hidden" />
@@ -131,12 +212,13 @@ return (
   Delete
 </button>
 </div>
-</div>))}
 
+</div>))}
 
   </ul>
   </div>
   </div>
+
 )
 
 }
